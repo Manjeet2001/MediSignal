17dcc3babf6c0d8801aa51bb03cb7addc7ea8ac8
Manjeet2001 <manjeetydv16@gmail.com>
2025-06-08 16:33:25 +0530

    Project setup

---
+++ src/main/java/com/mediSignal/MediSignal/MediSignalApplication.java
@@ -0,0 +1,14 @@
+package com.mediSignal.MediSignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MediSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSignalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediSignal/MediSignal/MediSignalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediSignal.MediSignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
